97e35a03dda46db476993672454e8afd528539a8
ManishSorte <mksorte1798@gmail.com>
2022-12-02 04:13:59 -0800

    Implemented one test case to check Jenkins pipeline triggering.

--- src/test/java/com/backend/social/socialbackendapis/SocialBackendApisApplicationTests.java
+++ src/test/java/com/backend/social/socialbackendapis/SocialBackendApisApplicationTests.java
@@ -1,13 +1,28 @@
 package com.backend.social.socialbackendapis;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.runner.RunWith;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SocialBackendApisApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(SocialBackendApisApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("Testing test class");
+		assertEquals(true,true);
 	}
 
 }
